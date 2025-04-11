0ce88bcce40c9ad17b87f2088736b67027686e56
Darshan <darshankumaravelk@gmail.com>
2025-04-11 21:56:49 +0530

    minor chages

--- src/test/java/com/github/darshan744/TestApplication/Main.java
+++ src/test/java/com/github/darshan744/TestApplication/Main.java
@@ -2,7 +2,6 @@ package com.github.darshan744.TestApplication;
 
 import io.github.darshan744.nebula.Nebula;
 import io.github.darshan744.nebula.Http.Constants.HttpMethod;
-import io.github.darshan744.nebula.Middleware.Handler.MiddlewareRegistry;
 import io.github.darshan744.nebula.Route.Router;
 
 public class Main {
@@ -10,8 +9,6 @@ public class Main {
         Nebula.start();
         UserController controller = new UserController();
         Router router = Router.getRouter();
-        MiddlewareRegistry registry = MiddlewareRegistry.getRegistry();
-        registry.registerMiddleware(null);
         //test for param
         router.registerRoute(HttpMethod.POST, "/users/{id}", controller);
         //test for queryparam
